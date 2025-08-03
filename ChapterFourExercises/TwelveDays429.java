3385ebf9a2e1bf575aed65662274aab14618d909
preshgerman3 <preshgerman3@gmail.com>
2025-08-03 14:33:29 +0100

    Add files via upload

---
+++ ChapterFourExercises/TwelveDays429.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class TwelveDays429 {
+    public static void main(String[] args) {
+        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter day: ");
+        int day = Integer.valueOf(scanner.nextLine());
+        if (!((day >= 1) && (day <= 12))) {
+            System.out.println("Choose between 1 and 12 inclusive!");
+        } else {
+            String opening = null;
+            switch (day) {
+                case 1:
+                    opening = "On the first day of Christmas, my true love sent to me";
+                    break;
+                case 2:
+                    opening = "On the second day of Christmas, my true love sent to me";
+                    break;
+                case 3:
+                    opening = "On the third day of Christmas, my true love sent to me";
+                    break;
+                case 4:
+                    opening = "On the fourth day of Christmas, my true love sent to me";
+                    break;
+                case 5:
+                    opening = "On the fifth day of Christmas, my true love sent to me";
+                    break;
+                case 6:
+                    opening = "On the sixth day of Christmas, my true love sent to me";
+                    break;
+                case 7:
+                    opening = "On the seventh day of Christmas, my true love sent to me";
+                    break;
+                case 8:
+                    opening = "On the eight day of Christmas, my true love sent to me";
+                    break;
+                case 9:
+                    opening = "On the ninth day of Christmas, my true love sent to me";
+                    break;
+                case 10:
+                    opening = "On the tenth day of Christmas, my true love sent to me";
+                    break;
+                case 11:
+                    opening = "On the eleventh day of Christmas, my true love sent to me";
+                    break;
+                default:
+                    opening = "On the twelfth day of Christmas, my true love sent to me";
+                    break;
+            } 
+            System.out.println();
+            System.out.println(opening);
+            
+            
+            for (int line = day; line >= 1; line--) {
+                String lyric;
+                switch (line) {
+                    case 1:
+                        lyric = "A partridge in a pear tree.";
+                        break;
+                    case 2:
+                        lyric = "Two turtle doves";
+                        break;
+                    case 3:
+                        lyric = "Three French hens,";
+                        break;
+                    case 4:
+                        lyric = "Four calling birds,";
+                        break;
+                    case 5:
+                        lyric = "Five golden rings,";
+                        break;
+                    case 6:
+                        lyric = "Six geese a-laying,";
+                        break;
+                    case 7:
+                        lyric = "Seven swans a-swimming,";
+                        break;
+                    case 8:
+                        lyric = "Eight maids a-milking,";
+                        break;
+                    case 9:
+                        lyric = "Nine ladies dancing,";
+                        break;
+                    case 10:
+                        lyric = "Ten lords a-leaping,";
+                        break;
+                    case 11:
+                        lyric = "Eleven pipers piping,";
+                        break;
+                    default:
+                        lyric = "Twelve drummers drumming,";
+                        break;
+                } 
+                System.out.println(lyric);
+            }
+
+        }
+        
+        
+        scanner.close();
+        
+    }
+}
